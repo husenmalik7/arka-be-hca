d94ed5958be5d8b6c7edf42b8ba5c225fc142bb8
husenmalik7 <husenmalik7@gmail.com>
2021-11-13 17:01:43 +0700

    cleaning the file

--- src/helper/form.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    success: (res, data) => { 
-        res.json ({ status: 200, msg: 'success', appears: data.length, data }); 
-    }
-
-    //this is a good one
-    //res.status(500).json({status: 404, message: 'error not found'}); 
-};
