dad1f2fbc1db90192eadac02f971c45a7644b341
husenmalik7 <husenmalik7@gmail.com>
2021-12-04 09:20:30 +0700

    login engineer

--- src/routes/engineer.js
+++ src/routes/engineer.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
+const auth = require("../helpers/auth");
+
 const controller = require("../controllers/engineer");
+const authController = require("../controllers/auth");
 
 const Router = express.Router();
 
 Router.post("/register", controller.registerEngineer);
+Router.post("/login", authController.login);
 
 module.exports = Router;
