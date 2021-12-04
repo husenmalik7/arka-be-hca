acb9cada953d324e923a1b0bb59558bf51c7a232
husenmalik7 <husenmalik7@gmail.com>
2021-12-04 08:49:10 +0700

    add cors setup

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const routes = require("./src/routes/index");
 app.use("/", routes);
